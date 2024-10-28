d7fa7874bdde5f0d55c2ac85c89c2b7b56c7c7b1
ireneuszpolec <ireneusz.polec@gmail.com>
2024-10-28 11:39:06 +0100

    wersja prod-dev

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "sprawdzenie php";
+$mode = getenv("APP_MODE");
+$key = getenv("API_KEY");
 
+if($mod=="prod") 
+{
+    echo "Wersja produkcyjna"; 
+}
+else
+{
+    echo "Wersja deweloperska";
+}
 ?>
